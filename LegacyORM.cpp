de0c01640851b5c01f3185ad7f7dea8857edd370
brokepep <vanrooyenpanda@gmail.com>
2023-10-09 15:26:04 +0200

    more in depth regex

--- LegacyORM.cpp
+++ LegacyORM.cpp
@@ -3,7 +3,7 @@
 
 bool LegacyORM::executeQuery(std::string query) {
     bool result = false;
-    std::regex pattern("^(?=.*SELECT.*FROM)(?!.*(?:CREATE|DROP|UPDATE|INSERT|ALTER|DELETE|ATTACH|DETACH)).*$");
+    std::regex pattern("^/(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s+\S.*\s+from\s+\S+)|(insert\s+into\s+\S+)|(update\s+\S+\s+set\s+\S+)|(delete\s+from\s+\S+)|(((drop)|(create)|(alter)|(backup))\s+((table)|(index)|(function)|(PROCEDURE)|(ROUTINE)|(SCHEMA)|(TRIGGER)|(USER)|(VIEW))\s+\S+)|(truncate\s+table\s+\S+)|(exec\s+)|(\/\*)|(--)))(\s*[\;]\s*)*)+)/i");
     if (std::regex_match(query, pattern)) {
         std::cout << "Executing valid query: " << query << std::endl;
         result = true;
